23ecae182cb5cf94721e4b596337104cd7e220df
Tannors Tablet <tziehm@comcast.net>
2016-01-22 09:34:48 -0700

    I attempted a poor idea

--- algorithms.java
+++ algorithms.java
@@ -3,7 +3,7 @@
 class Algorithms {
 
 	public static void main(String[] args) {
-		int[] cards = {7, 3, 11, 9, 2, 9, 2};
+		int[] cards = {7, 3, 11, 9, 2, 9, 2, 1};
 		int[] sorted = sort(cards);
 		String str = "";
 		for (int j = 0; j < sorted.length; j ++) {
@@ -12,7 +12,7 @@ class Algorithms {
 		}
 
 		System.out.println(str);
-		System.out.println(present(sorted, 19));
+		System.out.println(present(sorted, 17));
 
 	}
 
@@ -48,11 +48,37 @@ class Algorithms {
 
 		public static boolean present(int[] a, int b) {
 			boolean present = false;
-			for (int j = 0; j <= a.length; j ++) {
+			int temp = 0;
+			// System.out.println(a.length);
+			if (a.length % 2 == 0) {
+				// System.out.println("If is running");
+				for (int j = 0; j < a.length; j += 2) {
+					// System.out.println(a[j]);
+					if (a[j] <= b) {
+						if (a[j] == b) {
+							return present = true;
+						} else if (a[j-1] == b){
+							return present = true;
+						}
+					// temp = j;
+					// break;
+				}
+			}
+			System.out.println(temp);
+				// for (int k = temp + 1; k < a.length; k -= 1) {
+				// 	// System.out.println("Spot:" + a[k]);
+				// 	// System.out.println(b);
+				// 	if (a[k] == b) {
+				// 		 return present  = true;
+				// 	}
+			// }
+		} else {
+			for (int j = 0; j < a.length; j ++) {
 				if (a[j] == b) {
 					return present = true;
 				}
 			}
+		}
 			return present;
 		}
 	}
